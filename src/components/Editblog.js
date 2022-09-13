b70c19b9155bdc9221107b9c752100a503f3d62a
Bernardngari <bernardngari.ke@gmail.com>
2022-09-13 10:59:33 +0300

    Fix bug in edit functionality

--- src/components/Editblog.js
+++ src/components/Editblog.js
@@ -5,15 +5,15 @@ function Editblog({blog, onEditBlog}){
 		const[isEditing, setisEditing] = useState(false)
 
 	function handleEdit(e){
-		if(e.target.id === blog.id){
-			setisEditing(() => !isEditing)
+		if(e.target.id == blog.id){
+			setisEditing((isEditing) => !isEditing)
 		} else{
 			return isEditing
 		}
 	}
 	return (
 		<span onClick={handleEdit}>
-			{isEditing ? 
+			{isEditing? 
 			<Editform blog={blog} onEditBlog={onEditBlog} isEditing={isEditing} setisEditing={setisEditing}/>
 			: 
 			<span className='edit'><i className="fa-regular fa-pen-to-square" id={blog.id}>  edit</i></span> } 
